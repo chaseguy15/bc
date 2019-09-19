e4b91bbc47f702e970488730c9340eeb100a3148
Matthew Vu <54677355+mhvu2023@users.noreply.github.com>
2019-09-18 21:45:28 -0500

    File was not pushed with last commit

---
+++ obs_height/src/obj/point.rs
@@ -0,0 +1,15 @@
+pub struct Point {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Point {
+    pub fn new(x: f32, y: f32, z: f32) -> Self{
+        Self {
+            x,
+            y,
+            z,
+        }
+    }
+}
